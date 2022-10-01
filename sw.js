02ce2abf82df10c3264156aaef228c786c13bf06
TheJonaCode <mcjhona069@gmail.com>
2022-10-01 18:23:42 -0500

    Modificando la respuesta de petición Fetch

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
 self.addEventListener('fetch', event => {
 
-    if (event.request.url.includes('.jpg')) {
-        console.log(event.request.url);
+    // if (event.request.url.includes('.jpg')) {
+    //  console.log(event.request.url);
 
-        //let fotoReq = fetch('img/main.jpg');
-        // let fotoReq = fetch(event.request.url);
-        let fotoReq = fetch(event.request);
+    //let fotoReq = fetch('img/main.jpg');
+    // let fotoReq = fetch(event.request.url);
+    //let fotoReq = fetch(event.request);
 
-        event.respondWith(fotoReq);
+    //event.respondWith(fotoReq);
+    //}
+
+    if (event.request.url.includes('style.css')) {
+        let respuesta = new Response(`
+        body{
+            background-color: red !important;
+            color: blue;
+        }
+        `, {
+            headers: {
+                'Content-Type': 'text/css'
+            }
+        });
+
+        event.respondWith(respuesta);
     }
 });
